150b797ce35585500bac6c5117f52adc8a66b660
Aronstac <95855565+Aronstac@users.noreply.github.com>
2025-06-10 02:21:54 +0300

    Deleted App.test.tsx

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-it('renders dashboard after auth', async () => {
-  render(<App />);
-  const heading = await screen.findByRole('heading', { name: /dashboard/i });
-  expect(heading).toBeInTheDocument();
-});
